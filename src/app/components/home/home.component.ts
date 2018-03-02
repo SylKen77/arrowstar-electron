bd06c8cd49fb036149c6619f8a9eb13112c25be2
Maarten De Cock <mr.decock@gmail.com>
2018-03-02 14:56:11 +0100

    Read and write to file poc

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ElectronService} from '../../providers/electron.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private electronService: ElectronService) {
+  }
 
   ngOnInit() {
   }
 
+  public writeLine() {
+    console.log('HomeComponent.writeLine');
+    this.electronService.writeLine({naam: 'Maarten'});
+  }
+
+  public readLines() {
+    console.log('HomeComponent.readLines');
+    this.electronService.readLines();
+  }
 }
