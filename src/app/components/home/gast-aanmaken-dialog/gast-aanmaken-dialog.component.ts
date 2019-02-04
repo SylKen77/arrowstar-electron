01f09feec784079731c6e2205a62147e178f66a7
Maarten De Cock <mr.decock@gmail.com>
2019-02-04 17:08:55 +0100

    Layout

--- src/app/components/home/gast-aanmaken-dialog/gast-aanmaken-dialog.component.ts
+++ src/app/components/home/gast-aanmaken-dialog/gast-aanmaken-dialog.component.ts
@@ -7,7 +7,7 @@ import {ImageService} from '../../../services/image-service';
 @Component({
   selector: 'app-klant-aanmaken-dialog',
   templateUrl: './gast-aanmaken-dialog.component.html',
-  styleUrls: ['./gast-aanmaken-dialog.component.css']
+  styleUrls: ['./gast-aanmaken-dialog.component.scss']
 })
 export class GastAanmakenDialogComponent implements OnInit {
 
