4b3f17abdce5538173cac05a7a7866439228665c
mdcock7 <maarten.decock@vdab.be>
2023-12-13 09:34:12 +0100

    Business code weggehaald uit service en naar model gebracht (beter herbruikbaar voor als commands gesquashed moeten worden)

--- src/app/model/historiek.ts
+++ src/app/model/historiek.ts
@@ -1,4 +1,9 @@
 import {HistoriekJaar} from './historiek-jaar';
+import {Product} from './product';
+import {Klant} from './klant';
+import {Kassa} from './kassa';
+import {HistoriekTelling} from './historiek-telling';
+import {HistoriekAfsluiting} from './historiek-afsluiting';
 
 export class Historiek {
 
@@ -23,6 +28,78 @@ export class Historiek {
       this._jaren = [...this._jaren, historiekJaar];
     }
     return historiekJaar;
+  }
+
+  aankoopToevoegen(timestamp: Date, klant: Klant, product: Product): Historiek {
+    const historiekJaar = this.getOrCreateJaar(timestamp.getFullYear());
+    const bedrag = product.getPrijs(klant.klantType);
+
+    historiekJaar.getOrCreateVerkoopPerProduct(product).aantalVerkocht++;
+    historiekJaar.getOrCreateVerkoopPerProduct(product).bedrag += bedrag;
+
+    historiekJaar.getOrCreateVerkoopPerKlant(klant).aantalGekocht++;
+    historiekJaar.getOrCreateVerkoopPerKlant(klant).bedrag += bedrag;
+
+    return this;
+  }
+
+  aankoopVerwijderen(timestamp: Date, klant: Klant, product: Product): Historiek {
+    const historiekJaar = this.getOrCreateJaar(timestamp.getFullYear());
+    const bedrag = product.getPrijs(klant.klantType);
+
+    historiekJaar.getOrCreateVerkoopPerProduct(product).aantalVerkocht--;
+    historiekJaar.getOrCreateVerkoopPerProduct(product).bedrag -= bedrag;
+
+    historiekJaar.getOrCreateVerkoopPerKlant(klant).aantalGekocht--;
+    historiekJaar.getOrCreateVerkoopPerKlant(klant).bedrag -= bedrag;
+
+    return this;
+  }
+
+  klantAfrekenen(timestamp: Date, klant: Klant, kassa: Kassa, viaOverschrijving: boolean): Historiek {
+    const historiekJaar = this.getOrCreateJaar(timestamp.getFullYear());
+    const bedrag = klant.getSchuld();
+
+    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = kassa.saldo;
+    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = kassa.saldo;
+
+    historiekJaar.samenvatting.inkomsten += bedrag;
+    if (viaOverschrijving) {
+      historiekJaar.samenvatting.saldoOverschrijvingen += bedrag;
+    } else {
+      historiekJaar.samenvatting.saldoCash += bedrag;
+      historiekJaar.samenvatting.saldoEnd += bedrag;
+    }
+
+    return this;
+  }
+
+  kassaTellen(timestamp: Date, saldo: number, opmerking: string, kassa: Kassa): Historiek {
+    const historiekJaar = this.getOrCreateJaar(timestamp.getFullYear());
+    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = saldo;
+    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = saldo;
+
+    const verschil = saldo - kassa.saldo;
+    historiekJaar.samenvatting.saldoEnd += verschil;
+    historiekJaar.samenvatting.saldoTellingen += verschil;
+    historiekJaar.samenvatting.inkomsten += verschil;
+
+    historiekJaar.addTelling(new HistoriekTelling(timestamp, saldo, kassa.saldo, verschil));
+
+    return this;
+  }
+
+  kassaAfsluiten(timestamp: Date, bedrag: number, opmerking: string, kassa: Kassa): Historiek {
+    const historiekJaar = this.getOrCreateJaar(timestamp.getFullYear());
+    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = kassa.saldo;
+    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = kassa.saldo;
+
+    historiekJaar.samenvatting.saldoAfsluitingen += bedrag;
+    historiekJaar.samenvatting.saldoEnd -= bedrag;
+
+    historiekJaar.addAfluiting(new HistoriekAfsluiting(timestamp, bedrag, opmerking));
+
+    return this;
   }
 
 }
--- src/app/services/historiek-service.ts
+++ src/app/services/historiek-service.ts
@@ -8,8 +8,6 @@ import {KlantService} from './klant-service';
 import {KassaTellenCommand} from '../commands/kassa-tellen-command';
 import {KassaService} from './kassa-service';
 import {KassaAfsluitenCommand} from '../commands/kassa-afsluiten-command';
-import {HistoriekTelling} from '../model/historiek-telling';
-import {HistoriekAfsluiting} from '../model/historiek-afsluiting';
 import {ProductService} from './product-service';
 import {HistoriekInitCommand} from '../commands/historiek-init-command';
 
@@ -27,71 +25,33 @@ export class HistoriekService extends Store<Historiek> {
   }
 
   aankoopToevoegen(command: AankoopToevoegenCommand) {
-    const historiekJaar = this.state.getOrCreateJaar(command.timestamp.getFullYear());
     const klant = this.klantService.getKlant(command.klantId);
     const product = this.productService.getProduct(command.productId);
-    const bedrag = product.getPrijs(klant.klantType);
-
-    historiekJaar.getOrCreateVerkoopPerProduct(product).aantalVerkocht++;
-    historiekJaar.getOrCreateVerkoopPerProduct(product).bedrag += bedrag;
-
-    historiekJaar.getOrCreateVerkoopPerKlant(klant).aantalGekocht++;
-    historiekJaar.getOrCreateVerkoopPerKlant(klant).bedrag += bedrag;
+    const newHistoriek = this.state.aankoopToevoegen(command.timestamp, klant, product);
+    this.setState(newHistoriek);
   }
 
   aankoopVerwijderen(command: AankoopVerwijderenCommand) {
-    const historiekJaar = this.state.getOrCreateJaar(command.timestamp.getFullYear());
     const klant = this.klantService.getKlant(command.klantId);
     const product = this.productService.getProduct(command.productId);
-    const bedrag = product.getPrijs(klant.klantType);
-
-    historiekJaar.getOrCreateVerkoopPerProduct(product).aantalVerkocht--;
-    historiekJaar.getOrCreateVerkoopPerProduct(product).bedrag -= bedrag;
-
-    historiekJaar.getOrCreateVerkoopPerKlant(klant).aantalGekocht--;
-    historiekJaar.getOrCreateVerkoopPerKlant(klant).bedrag -= bedrag;
+    const newHistoriek = this.state.aankoopVerwijderen(command.timestamp, klant, product);
+    this.setState(newHistoriek);
   }
 
   afrekenen(command: KlantAfrekenenCommand) {
-    const historiekJaar = this.state.getOrCreateJaar(command.timestamp.getFullYear());
-    const bedrag = this.klantService.getKlant(command.klantId).getSchuld();
-
-    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = this.kassaService.state.saldo;
-    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = this.kassaService.state.saldo;
-
-    historiekJaar.samenvatting.inkomsten += bedrag;
-    if (command.viaOverschrijving) {
-      historiekJaar.samenvatting.saldoOverschrijvingen += bedrag;
-      console.log('saldoOverschrijvingen: ' + historiekJaar.samenvatting.saldoOverschrijvingen);
-    } else {
-      historiekJaar.samenvatting.saldoCash += bedrag;
-      historiekJaar.samenvatting.saldoEnd += bedrag;
-      console.log('saldoCash: ' + historiekJaar.samenvatting.saldoCash);
-    }
+    const klant = this.klantService.getKlant(command.klantId);
+    const newHistoriek = this.state.klantAfrekenen(command.timestamp, klant, this.kassaService.state, command.viaOverschrijving);
+    this.setState(newHistoriek);
   }
 
   kassaTellen(command: KassaTellenCommand) {
-    const historiekJaar = this.state.getOrCreateJaar(command.timestamp.getFullYear());
-    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = command.saldo;
-    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = command.saldo;
-
-    const verschil = command.saldo - this.kassaService.state.saldo;
-    historiekJaar.samenvatting.saldoEnd += verschil;
-    historiekJaar.samenvatting.saldoTellingen += verschil;
-    historiekJaar.samenvatting.inkomsten += verschil;
-
-    historiekJaar.addTelling(new HistoriekTelling(command.timestamp, command.saldo, this.kassaService.state.saldo, verschil));
+    const newHistoriek = this.state.kassaTellen(command.timestamp, command.saldo, command.opmerking, this.kassaService.state);
+    this.setState(newHistoriek);
   }
 
   kassaAfsluiten(command: KassaAfsluitenCommand) {
-    const historiekJaar = this.state.getOrCreateJaar(command.timestamp.getFullYear());
-    if (historiekJaar.samenvatting.saldoStart === 0) historiekJaar.samenvatting.saldoStart = this.kassaService.state.saldo;
-    if (historiekJaar.samenvatting.saldoEnd === 0) historiekJaar.samenvatting.saldoEnd = this.kassaService.state.saldo;
-
-    historiekJaar.samenvatting.saldoAfsluitingen += command.bedrag;
-    historiekJaar.samenvatting.saldoEnd -= command.bedrag;
-
-    historiekJaar.addAfluiting(new HistoriekAfsluiting(command.timestamp, command.bedrag, command.opmerking));
+    const newHistoriek = this.state.kassaAfsluiten(command.timestamp, command.bedrag, command.opmerking, this.kassaService.state);
+    this.setState(newHistoriek);
   }
 
 }
